84c960100b95ce2f468f761763113258afa2d63c
Jhonsisco <softwarejhon09@gmail.com>
2025-03-25 23:35:31 -0400

    Feat: Created Rectangulo class with method to calculate area
    Create a class called Rectangle with the base and height attributes.

---
+++ ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7;
+
+/**
+ *
+ * @author jhon
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        Rectangulo rectangulo1 = new Rectangulo(10,5);
+        
+        double area = rectangulo1.calcularArea();
+        System.out.println("El area del rectangulo es: " + area);
+    }
+}
