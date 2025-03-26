47196590b3c264857953e8c1fbccbaef661e73a2
Jhonsisco <softwarejhon09@gmail.com>
2025-03-25 23:15:40 -0400

    Create CuentaBancaria.java

---
+++ ejercicio14/src/main/java/com/mycompany/ejercicio14/CuentaBancaria.java
@@ -0,0 +1,34 @@
+package com.mycompany.ejercicio14;
+
+/**
+ *
+ * @author jhon
+ */
+public class CuentaBancaria { 
+    private String numeroCuenta; 
+    private double saldo; 
+
+    
+    public CuentaBancaria(String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
